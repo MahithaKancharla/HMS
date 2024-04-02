117b05ce4717e830bf77d0ce9973c70c05f14877
MahithaKancharla <98204725+MahithaKancharla@users.noreply.github.com>
2024-04-03 01:28:03 +0530

    TDD_testing

--- hostel/tests.py
+++ hostel/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import StudentInfo
 
-# Create your tests here.
+class ModelAttributeTest(TestCase):
+    def test_model_attributes_exist(self):
+        # Get all fields from the model
+        model_fields = [field.name for field in StudentInfo._meta.get_fields()]
+
+        # Check if all expected attributes exist
+        expected_attributes = ['department', 'gender','blood_group','joining_year','room_no','contact_number','address','email','father_name','mother_name','guardian_name']  # Add more attributes as needed
+        for attribute in expected_attributes:
+            self.assertIn(attribute, model_fields)
