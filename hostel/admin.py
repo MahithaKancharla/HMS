aad4ba5084b0e04cdab89eb2b41deefb0687ce15
MahithaKancharla <98204725+MahithaKancharla@users.noreply.github.com>
2024-04-02 23:59:03 +0530

    started creating models.py

--- hostel/admin.py
+++ hostel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from hostel.models import *
 
 # Register your models here.
+admin.site.register(StudentInfo)
--- hostel/models.py
+++ hostel/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models,connection
+from django import forms
+from .models import *
+from datetime import timedelta
+from datetime import time
+from datetime import date, datetime
 
 # Create your models here.
+class StudentInfo(models.Model):
+    roll_no = models.CharField(max_length=255,primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def  __str__(self):
+        return self.roll_no
