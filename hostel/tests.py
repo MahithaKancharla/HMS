266f5fce73088e867944280a77ffbf40a25a8f76
MahithaKancharla <98204725+MahithaKancharla@users.noreply.github.com>
2024-04-03 03:27:46 +0530

    TDD_testing

--- hostel/tests.py
+++ hostel/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import StudentInfo
+from .models import StudentInfo,WardenInfo
 
 class ModelAttributeTest(TestCase):
     def test_model_attributes_exist(self):
@@ -8,3 +8,9 @@ class ModelAttributeTest(TestCase):
         expected_attributes = ['department', 'gender','blood_group','joining_year','room_no','contact_number','address','email','father_name','mother_name']  # Add more attributes as needed
         for attribute in expected_attributes:
             self.assertIn(attribute, model_fields)
+
+class WardenInfoModelTest(TestCase):
+    def test_wardenn_info_exists(self):
+        wardens = WardenInfo.objects.count()
+
+        self.assertEqual(wardens,0)
